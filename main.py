03d4790daea45b1fea0f032d9e24b42ebdccf72c
Слон Девелопер <slonpashadevelop@gmail.com>
2026-06-13 02:13:58 +0500

    slon

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ class Point:
     color = "red"
     circle = 2
 
+    def __init__(self, a=0 , b=0):
+        self.x = a
+        self.y = b
+
+    def __del__(self):
+        print("Удаление" + str(self))
+
     def set_coords(self, x ,y ):
         self.x = x
         self.y = y
 
     def get_coords(self):
         return (self.x, self.y)
-pt = Point()
-pt.set_coords(1,2)
-f =getattr(pt, "get_coords")
-print(f())
+
+pt =Point()
+print(pt.__dict__)
