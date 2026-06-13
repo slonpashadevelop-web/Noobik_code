09357f9edae2061260c7c8fabb3380ff12c45692
Слон Девелопер <slonpashadevelop@gmail.com>
2026-06-13 08:40:28 +0500

    sasas

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
-class Point:
-    MAX_COORD = 100
-    MIN_COORD = 0
+class Server:
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def  set_coord(self, x, y):
-        if self.MIN_COORD <= x <= self.MAX_COORD:
-            self.x = x
-            self.y = y
-
-    def __getattribute__(self, item):
-        if item == 'x':
-            raise ValueError("Доступ Запрещен!")
-        else:
-            return object.__getattribute__(self, item)
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
 
     def __setattr__(self, key, value):
-        if key == "z":
-            raise AttributeError("Недоступ")
+        if key == "port" and not 0 <= value <= 65535:
+            raise ValueError("Port must be between 0 and 65535")
         else:
-         object.__setattr__(self, key, value)
+            object.__setattr__(self, key, value)
 
-    def __getattr__(self, item):
-        return False
-    def __delattr__(self, item):
-        print("asasas:" + item)
-        object.__delattr__(self, item)
 
-pt1 = Point(1, 2)
-pt =Point(3, 4)
-del pt1.x
+    def __getattr__(self, key):
+        return None
 
-print(pt1.__dict__)
+s = Server("localhost", 8080)
+print(s.port)      # 8080
+print(s.database)  # None
