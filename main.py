2b1b1131483b76fe5b4ca269d9c7be8f8395ea85
Слон Девелопер <slonpashadevelop@gmail.com>
2026-06-13 08:28:08 +0500

    asd

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-class Dog:
-    def __init__(self, name, age, breed):
-        self.name = name
-        self.age = age
-        self.breed = breed
+class Point:
+    MAX_COORD = 100
+    MIN_COORD = 0
 
-    def info(self):
-        print(self.name, self.age, self.breed)
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-    def birthday(self):
-        self.age += 1
-        print(f'{self.name} is {self.age} years old')
+    def  set_coord(self, x, y):
+        if self.MIN_COORD <= x <= self.MAX_COORD:
+            self.x = x
+            self.y = y
 
-d = Dog("Рекс", 3, "Овчарка")
-d.birthday()
-d.birthday()
+    def __getattribute__(self, item):
+        if item == 'x':
+            raise ValueError("Доступ Запрещен!")
+        else:
+            return object.__getattribute__(self, item)
+
+    def __setattr__(self, key, value):
+        if key == "z":
+            raise AttributeError("Недоступ")
+        else:
+         object.__setattr__(self, key, value)
+
+    def __getattr__(self, item):
+        return False
+    def __delattr__(self, item):
+        print("asasas:" + item)
+        object.__delattr__(self, item)
+
+pt1 = Point(1, 2)
+pt =Point(3, 4)
+del pt1.x
+
+print(pt1.__dict__)
